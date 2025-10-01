ee311f4f0b0d6a23074a2c53276a80239f8316e9
AidenCM1234 <aidenm124@nycstudents.net>
2025-10-01 09:33:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,18 @@ import random
 random_int = random.randint(1, 10)
 print(f"Random integer: {random_int}")
 
+guess_history=[]
 while win == 0:
     guess=(int(input("Guess a number 1-10: ")))
     if guess == random_int:
         print("You gamble succefuly")
         win=1
+        print(f"You guessed{guess_history}")
 
     elif guess > random_int:
-        print("Lower")
-
+        print(f"Lower then {guess}")
+        guess_history.append(guess)
+ 
     elif guess < random_int:
-        print("Higher")
+        print(f"Higher then {guess}")
+        guess_history.append(guess)
